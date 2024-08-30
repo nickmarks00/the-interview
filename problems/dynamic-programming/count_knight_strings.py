88f5ef282fb27cf1c7ccd62175c799efc31fc9bb
Nick Marks <nickmarks00@gmail.com>
2024-08-31 07:25:28 +1000

    Fix formatting issue

--- problems/dynamic-programming/count_knight_strings.py
+++ problems/dynamic-programming/count_knight_strings.py
@@ -3,9 +3,12 @@ COUNT KNIGHT STRINGS
 FROM: TikTok LiveNet Backend R&D Engineer Final Interview
 LANGUAGE: Python
 
-Consider a 4x4 grid containing characters 'a' to 'p'. Given a starting location (i, j) on the grid and a password length L, calculate the number of possible passwords that can be generated of length L.
+Consider a 4x4 grid containing characters 'a' to 'p'. Given a starting location
+(i, j) on the grid and a password length L, calculate the number of possible
+passwords that can be generated of length L.
 
-You can only move between letters in the way a chess knight does - two squares vertically/horizontally, then one square horizontall/vertically.
+You can only move between letters in the way a chess knight does - two squares
+vertically/horizontally, then one square horizontall/vertically.
 """
 
 
